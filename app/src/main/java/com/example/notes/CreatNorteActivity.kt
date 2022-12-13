d1d6aa73aee3ec42e6a56fdb54bc60708ebe2e52
Sold-Uot <66266458+Sold-Uot@users.noreply.github.com>
2022-12-13 21:34:30 +0300

    new commit

---
+++ app/src/main/java/com/example/notes/CreatNorteActivity.kt
@@ -0,0 +1,11 @@
+package com.example.notes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreatNorteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_creat_norte)
+    }
+}
